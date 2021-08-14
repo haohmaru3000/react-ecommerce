32c18d4e7a2ddf02395f465cda6e013376da3887
tuanit168 <tuanit168@gmail.com>
2021-08-14 17:20:11 +1000

    Build Product Screen

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,21 @@
+import Rating from "./Rating";
+
+function Product(props) {
+   const {product} = props;
+   return (
+      <div className="card">
+         <a href={`/product/${product._id}`}>
+            <img className="medium" src={product.image} alt="product" />
+         </a>
+         <div className="card-body">
+            <a href={`/product/${product._id}`}>
+               <h2>{product.name}</h2>
+            </a>
+            <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+            <div className="price">${product.price}</div>
+         </div>
+      </div>
+   );
+}
+
+export default Product;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import data from "../data";
+import Product from "../components/Product";
+
+function HomeScreen(props) {
+   return (
+      <div>
+         <div className="row center">
+            {data.products.map((product) => (
+               <Product key={product._id} product={product}></Product>
+            ))}
+         </div>
+      </div>
+   );
+}
+
+export default HomeScreen;
