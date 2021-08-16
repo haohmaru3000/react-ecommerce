64de48c12f06f56446c812d30a8d128acc8fa3bc
tuanit168 <tuanit168@gmail.com>
2021-08-16 16:31:33 +1000

    Load Products From Backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import data from "../data";
+import React, { useEffect, useState } from 'react';
 import Product from "../components/Product";
+import axios from 'axios';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
 function HomeScreen(props) {
+   const [products, setProducts] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const [error,setError] = useState(false);
+   useEffect(() => {
+      const fetchData = async() => {
+         try {
+            setLoading(true);
+            const {data} = await axios.get('/api/products');
+            setLoading(false);
+            setProducts(data);
+         } catch(err){
+            setError(err.message);
+            setLoading(false);
+         }
+      };
+      fetchData();
+   }, []);
    return (
       <div>
-         <div className="row center">
-            {data.products.map((product) => (
-               <Product key={product._id} product={product}></Product>
-            ))}
-         </div>
+         {loading? ( 
+            <LoadingBox></LoadingBox>
+         )  : error ? (
+            <MessageBox variant="danger">{error}</MessageBox>
+         ) : (
+            <div className="row center">
+               {products.map((product) => (
+                  <Product key={product._id} product={product}></Product>
+               ))}
+            </div>
+         )}
       </div>
    );
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -39,7 +39,7 @@ function ProductScreen(props) {
                            {product.countInStock > 0 ? (
                               <span className="success">In Stock</span>
                            ) : (
-                              <span className="error">Unavailable</span>
+                              <span className="danger">Unavailable</span>
                            )
                            }
                         </div>
